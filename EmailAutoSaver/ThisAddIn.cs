d5ce745c325fb92ea8a06b26b6a10ef765f8cf19
Michael Zhang <michaelz@vxsport.com>
2020-10-13 19:19:52 +1300

    initial upload

---
+++ EmailAutoSaver/ThisAddIn.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Outlook;
+using System.Text.RegularExpressions;
+
+namespace EmailAutoSaver
+{
+    public partial class ThisAddIn
+    {
+        List<Items> _taskItems = new List<Items>();
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Outlook.MAPIFolder inbox = Application.Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+            var jobFolder = inbox.Folders["Jobs"];
+            // dynamically attach an event when user drag item into the folder to trigger file saving to local disk..
+            foreach (Outlook.Folder job in jobFolder.Folders)
+            {
+                foreach (Outlook.Folder task in job.Folders)
+                {
+                    _taskItems.Add(task.Items);                   
+                }             
+            }
+            foreach (Items task in _taskItems)
+            {
+                task.ItemAdd += new Outlook.ItemsEvents_ItemAddEventHandler(AddItem);
+            }
+        }
+
+        private void AddItem(object item)
+        {
+            // Step 1. fetch folder path etc..
+            var msg = item as MailItem;
+            var fdr = msg.Parent as Folder;
+            var folders = fdr.FolderPath.Split('\\').ToList();
+            // gets the last two job/task name
+            var jobName = folders[folders.Count -2];
+            var taskName = folders.Last();
+            // Step 2. Save email to disk
+            var dir = @"C:\Jobs\";
+            try
+            {
+                string jobFolderPath = Path.Combine(dir, jobName);
+                if (!Directory.Exists(jobFolderPath))
+                {
+                    Directory.CreateDirectory(jobFolderPath); // create if not exists..
+                }
+                // check if folder exist
+                var destPath = Path.Combine(dir, jobName, taskName);
+                if (!Directory.Exists(destPath))
+                {
+                    Directory.CreateDirectory(destPath); // create if not exists..
+                }
+                // task folder exists, save the email
+                string msgTitle = CleanupMessageTitle(msg.Subject);
+                var fileName = GetFileName(destPath + @"\" + msgTitle, 0);
+                msg.SaveAs(fileName);
+            }
+            catch (System.Exception e)
+            {
+                //MessageBox.Show("Path not exist or network driver not connected...");
+            }
+        }
+
+        private string CleanupMessageTitle(string subject)
+        {
+            var rgx = new Regex(@"\||\/|\<|\>|""|:|\*|\\|\?");
+            return rgx.Replace(subject, "");
+        }
+
+        private string GetFileName(string name, int copy)
+        {
+            string fileName = name + (copy == 0 ? "" : "("+ copy.ToString() + ")") + ".msg" ;
+            if (File.Exists(fileName)) {
+                copy++;
+                fileName = GetFileName(name, copy);
+             }
+            return fileName;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
